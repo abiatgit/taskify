0f36332e0854a5e923ec5914341f736deda65336
Abi George <contactabisgeorge@gmail.com>
2025-02-06 10:12:35 +0000

    use action handler

---
+++ actions/create-board/index.ts
@@ -0,0 +1,41 @@
+
+
+"use server"
+
+import { db } from "@/lib/db"
+import {auth} from "@clerk/nextjs/server"
+import { revalidatePath } from "next/cache"
+import { CreateBoard } from "./schema"
+import { InputType,ReturnType } from "./type"
+import{createSafeAction} from "@/lib/create-safe-action"
+
+
+export const handler=async (data:InputType):Promise<ReturnType> =>{
+  const {userId} = await auth()
+
+     if(!userId){
+      return {
+        error:"Unauthorized"
+      }
+     }
+     const{title}=data
+     let board;
+try{
+   board =await db.board.create({
+    data:{
+      title
+    }
+  })
+}
+catch(error){
+  console.log(error)
+   return {
+    error:"Failed to create"
+   }
+}
+revalidatePath(`/board/${board.id}`)
+return {data:board}
+}
+
+
+export const cretaeBoard= createSafeAction(CreateBoard,handler)
---
+++ hooks/use-action.ts
@@ -0,0 +1,58 @@
+import { useState,useCallback } from "react";
+import { ActionState,fieldErrors } from "@/lib/create-safe-action";
+
+ export type Action<TInput,TOutput> =(data:TInput)=>
+Promise<ActionState<TInput,TOutput>>
+
+
+ interface UseActionOptions<TOutput> {
+    onSuccess?: (data: TOutput) => void;
+    onError?: () => void;
+    onComplete?: () => void;
+ }
+ export const UseAction=<TInput,TOutput>(
+    action:Action<TInput,TOutput>,
+    option:UseActionOptions<TOutput> = {
+
+    }
+ )=>{
+   const [fieldErrors,setFieldErrors]=useState<fieldErrors<TInput> | undefined>(undefined)
+   const [error,setError]=useState<string | undefined>(undefined)
+   const [data,setData]=useState<TOutput | undefined>(undefined)
+   const [isLoading,setIsLoading]=useState<boolean>(false)
+
+   const execute=useCallback(
+    async(input :TInput)=>{
+        setIsLoading(true)
+        try{
+const result =await action(input)
+if(!result){
+    return 
+}
+if(result.fieldErrors){
+    setFieldErrors(result.fieldErrors)
+}
+if(result.error){
+    setError(result.error)
+}
+if(result.data){
+    setData(result.data)
+    option.onSuccess?.(result.data)
+}
+
+}
+finally{
+    setIsLoading(false)
+   
+        option.onComplete?.()
+    
+}
+    },[action,option]
+   );
+   return{
+    execute,
+    fieldErrors,
+    error,
+    data,isLoading
+   }
+ }
