10229fe11874f10f2ebfcc210ec2408f3aad0550
Abi George <contactabisgeorge@gmail.com>
2025-02-08 14:32:38 +0000

    meta data title dynamically change

--- app/(platform)/(dashboard)/organization/[organizationId]/layout.tsx
+++ app/(platform)/(dashboard)/organization/[organizationId]/layout.tsx
@@ -1,4 +1,14 @@
+
+import {startCase,} from "lodash"
 import OrgControl from "./_componets/org-control";
+import { auth } from "@clerk/nextjs/server";
+export async function generateMetadata() {
+  const {orgSlug}= await auth()
+  return{
+    title:startCase(orgSlug|| "organization")
+  }
+  
+}
 const OrganizationIdLayout = ({ children }: { children: React.ReactNode }) => {
   return (
     <>
